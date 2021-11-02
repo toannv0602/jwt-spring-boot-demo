927395205a1fddd03dc4707523e110fdf19cc499
Ngo Van Toan <toante2000@gmail.com>
2021-11-02 17:08:36 +0700

    commit

--- src/main/java/com/example/jwtspringbootdemo/services/UserDetailsServiceImpl.java
+++ src/main/java/com/example/jwtspringbootdemo/services/UserDetailsServiceImpl.java
@@ -5,13 +5,14 @@ import com.example.jwtspringbootdemo.responsitory.UserReponsitory;
 import com.example.jwtspringbootdemo.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     UserReponsitory userReponsitory;
